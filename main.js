aeda81f5e015c0ffa2e886d5c7fa7b54268177fa
jacebeep <61972817+jacebeep@users.noreply.github.com>
2026-07-31 11:19:16 -0400

    Added anti-copy and source code protection engine

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// --- ANTI-COPY & SOURCE CODE PROTECTION ENGINE ---
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (
+    e.keyCode === 123 || 
+    (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74 || e.keyCode === 67)) || 
+    (e.ctrlKey && (e.keyCode === 85 || e.keyCode === 83))
+  ) {
+    e.preventDefault();
+    return false;
+  }
+});
+
 const PRODUCTS = [
   { 
     id: 17, 
