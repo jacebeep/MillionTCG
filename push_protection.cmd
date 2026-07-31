75f380d4e04d3f18db12b471d42dbd0a86972c9e
jacebeep <61972817+jacebeep@users.noreply.github.com>
2026-07-31 11:19:58 -0400

    Delete push_protection.cmd

--- push_protection.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Users\jacep\AppData\Local\GitHubDesktop\app-3.5.11\resources\app\git\cmd
-git add -A
-git commit -m "Added anti-copy and source code protection engine"
-git push origin main
-git push origin gh-pages --force
